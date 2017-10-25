55361dbe7fa6d5775f61d03880136c27609797cd
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-10-25 11:04:44 -0600

    Initial commit

---
+++ src/cs2130p3/DProb.java
@@ -0,0 +1,25 @@
+package cs2130p3;
+
+public class DProb {
+	
+	public static long Permutation(long N, long X) {
+		
+	}
+	
+	public static long Combination(long n, long X) {
+		
+	}
+	
+	public static long HyperGeometric(long Np, long Xp, long N, long X) {
+		
+	}
+	
+	public static long Binomial(double P, long N, long X) {
+		
+	}
+	
+	public static long Poisson(double Xmean, long X) {
+		
+	}
+
+} // end class
