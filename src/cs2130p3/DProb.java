f672ff316bd26e9a51ac082d1cb1ed2da6695450
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-10-25 18:53:20 -0600

    Completed permutation function and started the PartB test file

--- src/cs2130p3/DProb.java
+++ src/cs2130p3/DProb.java
@@ -1,25 +1,46 @@
 package cs2130p3;
 
+import java.lang.Math;
+
 public class DProb {
 	
 	public static long Permutation(long N, long X) {
+	    double permutation = 0.0;
+	    double n = 1.0;
+	    double r = 1.0;
+	    double nr = N - X;
 		
+	    for (int i = 0; i < N; i++) {
+	        n =  n * (double) (N - i);
+	    }
+	    
+       for (int i = 0; i < nr; i++) {
+            r =  r * (double) (nr - i);
+        }
+
+	    permutation = Math.round((n/r));
+    
+	    return (long) permutation;
 	}
 	
-	public static long Combination(long n, long X) {
-		
+	public static long Combination(long N, long X) {
+
+	    return 0;
 	}
 	
 	public static long HyperGeometric(long Np, long Xp, long N, long X) {
-		
+	    
+	    return 0;
 	}
 	
 	public static long Binomial(double P, long N, long X) {
-		
+	    
+		return 0;
 	}
 	
 	public static long Poisson(double Xmean, long X) {
-		
+	    
+		return 0;
 	}
 
 } // end class
